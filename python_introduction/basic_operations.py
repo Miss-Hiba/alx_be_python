dcbb52e6711f692174c2a1d44d945700914286b1
Miss-Hiba <mshiba2207@gmail.com>
2025-01-15 12:45:06 +0100

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1= 10 
+number2= 5
+sum= number1 +number2
+print("Addition of" ,number1 ,"and" ,number2 ,"is" ,sum)
+sub= number1 - number2
+print("Subtraction of" ,number1 ,"and" ,number2 ,"is" ,sub)
+mult= number1 * number2
+print("Multiplication of" ,number1 ,"and" ,number2 ,"is" ,mult)

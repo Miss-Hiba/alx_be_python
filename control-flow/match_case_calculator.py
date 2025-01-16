7f050e0a10205e09d6b948b9dee6490559b9880e
Miss-Hiba <mshiba2207@gmail.com>
2025-01-16 17:14:18 +0100

    match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+operation=input("Choose the operation (+, -, *, /):")
+if operation=="+":
+    result=num1+num2
+elif operation=="-":
+    result=num1-num2
+elif operation=="*":
+    result=num1*num2
+else:
+    if num2!=0:
+        result=num1/num2
+    else :
+        print("non valide")
+print(result)
